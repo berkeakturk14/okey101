eb6859439737390a344bb68f476541efb66af153
Berke Aktürk <berkeakturk14@gmail.com>
2026-07-28 19:21:10 +0300

    2 okey kilidi duzeldi + modern bildirimler

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3495767080" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3833167536" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
