0c1f5deb4dc77d29e19b7813d06bb96796cb9363
Berke Aktürk <berkeakturk14@gmail.com>
2026-08-07 18:33:02 +0300

    Esli mod eklendi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2958071112" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2685543319" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
